c1cbe9cdca480a960e7b2a110e7e6a76863ab21a
nyxonman <shakya.nikeshman@gmail.com>
2020-08-20 09:22:49 +0200

    bufmgr graphs, colors arr added

--- monitorTrace.py
+++ monitorTrace.py
@@ -599,6 +599,45 @@ prim_code_str = {
 
 }
 
+colors_list = [
+        "#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+        "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+        "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+        "#83AB58", "#001C1E", "#D1F7CE", "#004B28", "#C8D0F6", "#A3A489", "#806C66", "#222800",
+        "#BF5650", "#E83000", "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+        "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94", "#7ED379", "#012C58",
+
+        "#7A7BFF", "#D68E01", "#353339", "#78AFA1", "#FEB2C6", "#75797C", "#837393", "#943A4D",
+        "#B5F4FF", "#D2DCD5", "#9556BD", "#6A714A", "#001325", "#02525F", "#0AA3F7", "#E98176",
+        "#DBD5DD", "#5EBCD1", "#3D4F44", "#7E6405", "#02684E", "#962B75", "#8D8546", "#9695C5",
+        "#E773CE", "#D86A78", "#3E89BE", "#CA834E", "#518A87", "#5B113C", "#55813B", "#E704C4",
+        "#00005F", "#A97399", "#4B8160", "#59738A", "#FF5DA7", "#F7C9BF", "#643127", "#513A01",
+        "#6B94AA", "#51A058", "#A45B02", "#1D1702", "#E20027", "#E7AB63", "#4C6001", "#9C6966",
+        "#64547B", "#97979E", "#006A66", "#391406", "#F4D749", "#0045D2", "#006C31", "#DDB6D0",
+        "#7C6571", "#9FB2A4", "#00D891", "#15A08A", "#BC65E9", "#FFFFFE", "#C6DC99", "#203B3C",
+
+        "#671190", "#6B3A64", "#F5E1FF", "#FFA0F2", "#CCAA35", "#374527", "#8BB400", "#797868",
+        "#C6005A", "#3B000A", "#C86240", "#29607C", "#402334", "#7D5A44", "#CCB87C", "#B88183",
+        "#AA5199", "#B5D6C3", "#A38469", "#9F94F0", "#A74571", "#B894A6", "#71BB8C", "#00B433",
+        "#789EC9", "#6D80BA", "#953F00", "#5EFF03", "#E4FFFC", "#1BE177", "#BCB1E5", "#76912F",
+        "#003109", "#0060CD", "#D20096", "#895563", "#29201D", "#5B3213", "#A76F42", "#89412E",
+        "#1A3A2A", "#494B5A", "#A88C85", "#F4ABAA", "#A3F3AB", "#00C6C8", "#EA8B66", "#958A9F",
+        "#BDC9D2", "#9FA064", "#BE4700", "#658188", "#83A485", "#453C23", "#47675D", "#3A3F00",
+        "#061203", "#DFFB71", "#868E7E", "#98D058", "#6C8F7D", "#D7BFC2", "#3C3E6E", "#D83D66",
+
+        "#2F5D9B", "#6C5E46", "#D25B88", "#5B656C", "#00B57F", "#545C46", "#866097", "#365D25",
+        "#252F99", "#00CCFF", "#674E60", "#FC009C", "#92896B"]
 
 def check_and_install_package(pkg_list):
     """ check and installs the packages present in the list pkg_list """
@@ -1127,6 +1166,12 @@ def signal_handler(sig, frame):
     glob["QUIT"] = True
     raise SystemExit
 
+def get_n_colors(n):
+    ret = []
+    r = min(int(random.random() *256) - n -1,0)
+    for i in range(n):
+        ret.append(colors_list[r+i])
+    return ret
 
 def get_colors(n):
     ret = []
@@ -1850,6 +1895,117 @@ def graph_it():
         )
 
         fig.show()
+    # buffer get/release
+    if '0' in graph_ans_list or '4' in graph_ans_list:
+
+        bufmgr_df = pd.DataFrame()
+        timings_plot_df = pd.DataFrame()
+        filter_list = [19, 20]
+        bufmgr_df = csv_df[csv_df.tracecode_dec.isin(filter_list)]
+
+        trace_list = list(set(list(bufmgr_df.tracecode_dec)))
+        if not set(filter_list).issubset(trace_list):
+            print("All required traces {} are not present. Cannot draw the graph".format(filter_list))
+            return
+
+        bufmgr_df['buffer'] = bufmgr_df.trace_info.str[-4:]
+        bufmgr_df['buffer_dec'] = bufmgr_df.buffer.apply(int, base=16)
+        # print(bufmgr_df[['tracecode_dec','buffer']].head(30))
+        # print(bufmgr_df.head(10))
+
+        bufmgr_get_df = bufmgr_df[bufmgr_df["tracecode_dec"] == 19][['byte','frt_dec', 'trace_info','buffer', 'buffer_dec']]
+
+        bufmgr_get_df['owner'] = bufmgr_get_df.trace_info.str.split().str[2].str.split("(").str[0]
+        bufmgr_get_df['owner'] = bufmgr_get_df.owner.str[4:]
+        bufmgr_get_df['subfunc'] = bufmgr_get_df.trace_info.str.split().str[4]
+        bufmgr_get_df.reset_index(inplace=True)
+
+        # to get the rel frt
+        def buf_filter(x):
+            mydf = bufmgr_df[bufmgr_df.frt_dec > x.frt_dec]
+            index= mydf[mydf.buffer==x.buffer].first_valid_index()
+
+            if index:
+                return mydf.at[index, 'frt_dec']
+            else:
+                return np.nan
+
+        bufmgr_get_df['rel_frt'] = bufmgr_get_df.apply(buf_filter,axis=1)
+        bufmgr_get_df['frt_diff'] = (bufmgr_get_df['rel_frt'] - bufmgr_get_df['frt_dec'])
+        bufmgr_get_df['frt_diff'] = bufmgr_get_df['frt_diff'].astype('str')
+
+        bufmgr_get_df['info'] = bufmgr_get_df.owner + " 0x" + bufmgr_get_df.buffer + " "+ bufmgr_get_df.frt_diff.astype(str) + "us"
+        # bufmgr_get_df['frt_diff']
+
+        unique_owners = bufmgr_get_df.owner.unique()
+
+        buf_owner_dic = dict()
+        for owner in unique_owners:
+            buf_owner_dic[owner]=(bufmgr_get_df[bufmgr_get_df['owner'].str.match(owner)])[['frt_dec','owner','buffer_dec','rel_frt','info']]
+
+        # print(px.colors.qualitative.Dark24)
+        buff_colors = px.colors.qualitative.Dark24;
+        # buff_colors = get_n_colors(len(unique_owners));
+
+
+        fig = go.Figure()
+
+        shapes = list()
+        i=0
+
+        # per owner graphing
+        for key, owner_df in buf_owner_dic.items():
+            # print(key, owner_df)
+
+            # draw a scatter
+            fig.add_scatter(
+                x=owner_df['frt_dec'], y=owner_df['buffer_dec'],
+                mode="markers",
+                hoverinfo='text',
+                hovertext=owner_df['info'],
+                name = key,
+                marker=dict(
+                    color=buff_colors[i],
+                    line=dict(width=1,
+                              ))
+            )
+
+            # draw a rectangle
+            tuple_val = list(zip(owner_df.frt_dec,owner_df.rel_frt, owner_df.buffer_dec))
+            for get_frt,rel_frt, height in tuple_val:
+                shapes.append({'type': 'rect',
+                            'x0': get_frt,
+                            'y0': 0,
+                            'x1': rel_frt,
+                            'y1': height,
+                            # 'fillcolor':colors_list[i+1],
+                            'fillcolor':buff_colors[i],
+                            'opacity':0.5
+                            })
+
+            i=i+1
+
+
+        fig.update_layout(
+            title="Buffer",
+            showlegend=True,
+            # hovermode='x',
+            shapes=shapes,
+        )
+        fig.show()
+
+        buf_summary_df = (bufmgr_get_df.groupby('owner').count()).reset_index()[['owner','frt_dec','rel_frt']]
+        # fig.add_trace(go.bar(buf_summary_df, x="owner", y=["frt_dec","rel_frt"], barmode='group',), row=2, col=1)
+        # fig.add_trace(go.Bar(x=buf_summary_df.owner, y=buf_summary_df[["frt_dec"]]), row=2, col=1)
+        fig = px.bar(buf_summary_df, x="owner", y=["frt_dec","rel_frt"],
+              barmode='group',
+             )
+        # # print(fig)
+        fig.update_layout(barmode='group')
+        fig.show()
+
+        # count of buffers get and released
+
 
 
 cl_id = 0
@@ -2018,7 +2174,7 @@ if __name__ == "__main__":
             1: "RTT Between CL DataGet Req/Cnf",
             2: "CL Timings",
             3: "Timeline Visualizer",
-            4: "Buffer Claim/Release",
+            4: "Buffer Get/Release",
         }
 
         for key, val in graph_dic.items():
